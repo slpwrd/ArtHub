66129e0c889cfe03872dc04493607b59efd22476
slpwrd <sleepywardy@mail.ru>
2025-05-05 20:30:02 +0300

    0505work

---
+++ ArtHub_vue/resources/js/components/app.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+      <Header />
+      <main>
+        <h1>Welcome to ArtHub!</h1>
+        <p>Your gateway to art and culture.</p>
+      </main>
+      <Footer />
+    </div>
+  </template>
+  
+  <script>
+  import Header from '/header.vue';
+  import Footer from '/footer.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Footer,
+    },
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
+  
+  main {
+    margin: 20px;
+  }
+  </style>
+  
